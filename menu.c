29cd1a90a44c1f04c9e8373732c5cc1918f643a6
Christopher Vollick <psycotica0@gmail.com>
2008-12-10 20:37:22 -0500

    Comma Support Added.
    I've made some fairly largish changes.
    Now the whole interpretting thing is in a while loop now.
    
    It should be about the same for all the single commands, but the other ones work too now.
    
    Also, an error is spit out when an invalid command is run, rather than ignoring it.
    
    Also, I've split chomp out into it's own function.
    I was running into variable collisions with end and wasn't looking forward to making another stupid variable and the top of my main, and I was like
    "Man I wish there was, like, some kind of restricted scope that would do one thing then go away"
    Then I realized I was really dumb. Again.
    
    I haven't really looked into it, but some more things might find themselves in functions now that I've remembered they exist.

--- menu.c
+++ menu.c
@@ -13,12 +13,24 @@
 #define CURRENT_MAX 150
 #define INPUT_MAX 50
 
+/* This function takes the newline off a string, if it exists */
+void chomp (char * inp) {
+	char * end;
+	end = strchr(inp,'\n');
+	if(end != 0) {
+		*end='\0';
+	}
+}
 int main (int argc, char ** argv ) {
 
 	/* manymany decls */
 	char (*result)[CURRENT_MAX];
 	char inp[INPUT_MAX];
 	char * end;
+	char * iterator;
+	/* This variable is used to hold the character that will start a line*/
+	/* Used in the ',' causing newline and '+' causing space case */
+	char lineStarter=0;
 	FILE * tty;
 	int num=DEFAULT_NUM;
 	int max=0;
@@ -68,31 +80,61 @@ int main (int argc, char ** argv ) {
 	fgets(inp,INPUT_MAX,tty);
 	fclose(tty);
 
-	/* Try to convert user input to int */
-	selection=strtol(inp,&end,10);
-	if(end == inp) {
-		/* Not an int */
-		/* Commands Go Here */
-		if(*end == 'a') {
-			/* The 'a' command outputs every line*/
-			for(i=0;i<max; i++){
-				printf("%s",result[i]);
+	/* Initialize the iterator */
+	iterator=inp;
+	/* This is what I seem to get on a blank line */
+	if(*iterator != (char)10) {
+		while(*iterator != '\0') {
+			/* Try to convert user input to int */
+			selection=strtol(iterator,&end,10);
+
+			if(end == iterator) {
+				/* Not an int */
+
+				/* Consume White Space */
+				while(*end == ' ') end++;
+
+				/* Commands Go Here */
+				if(*end == '\0' || *end == (char)10) {
+					/* We're Done Here */
+					break;
+					/* I love the null command */
+				} else if(*end == 'a' && lineStarter==0) {
+					/* The 'a' command outputs every line*/
+					/* 'a' is only valid if it's the first, thus the lineStarter check*/
+					for(i=0;i<max; i++){
+						printf("%s",result[i]);
+					}
+					/* After a, no more input is accepted */
+					break;
+				} else if (*end == ',') {
+					/* The ',' command is one of those things that I love */
+					/* At first it doesn't seem like a command, but really it is. */
+					lineStarter='\n';
+					/* Set iterator to the next char */
+					iterator=end+1;
+				} else {
+					/* If I don't recognize it, end */
+					fprintf(stderr,"Unrecognized Command %c\n", *end);
+					break;
+				}
+			} else {
+				/* Selection is the index the user chose
+					 check if the index is valid. */
+				if(selection < max && selection >= 0) {
+					/* chomp */
+					chomp(result[selection]);
+					/* print selected item to STDOUT */
+					if (lineStarter!=0) {
+						putchar(lineStarter);
+					}
+					printf(result[selection]);
+				} else {
+					/* User selected a bad index */
+					fprintf(stderr, "There is no item with the index %d\n", selection);
+				}
+				iterator=end;
 			}
-		}
-	} else {
-		/* Selection is the index the user chose
-		   check if the index is valid. */
-		if(selection < max && selection >= 0) {
-			/* chomp */
-			end = strchr(result[selection],'\n');
-			if(end != 0) {
-				*end='\0';
-			}
-			/* print selected item to STDOUT */
-			printf(result[selection]);
-		} else {
-			/* User selected a bad index */
-			fprintf(stderr, "There is no item with the index %d\n", selection);
 		}
 	}
 
